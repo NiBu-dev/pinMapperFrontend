7f447212ce8776746f85a16e951fc82ee3bb0b3f
Nick <sahas023@gmail.com>
2020-01-18 13:26:15 +0100

    removed backend

--- backend/models/microcontroller.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const subscriberSchema = new mongoose.Schema({
-    test: {
-        type: String,
-        required: true
-    },
-});
-
-module.exports = mongoose.model('Microcontroller', subscriberSchema);
