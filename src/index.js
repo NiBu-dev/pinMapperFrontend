965f671c6ea09a9924ec5f3fd5f3ca845af88554
Nick <sahas023@gmail.com>
2020-01-18 21:21:37 +0100

    conditional set axios based on NODE_ENV

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
-axios.defaults.baseURL = "http://localhost:3000";
+console.log('env is', process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+    axios.defaults.baseURL = "http://localhost:3000";
+} else if (process.env.NODE_ENV === 'production') {
+    axios.defaults.baseURL = "https://pin-pin-mapper.herokuapp.com/:3000";
+}
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
